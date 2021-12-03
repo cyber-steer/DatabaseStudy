58dd181b281f96bd5e25a53552fa4874cb8d8f61
name <cybersteer@naver.com>
2021-12-03 21:52:35 +0900

    create table

---
+++ LPGProject.sql
@@ -0,0 +1,11 @@
+drop table sale;
+drop table buy;
+drop table charge;
+drop table base;
+drop table bowl;
+drop table customer;
+drop table employee;
+drop table station;
+drop table payment;
+drop table gas;
+
